91f1e4a6cad7d8df3d1f63ce56c1760f5f3f9ab9
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2023-09-11 14:33:29 -0300

    Separando o html do php

--- src/Controller/VideoFormController.php
+++ src/Controller/VideoFormController.php
@@ -18,36 +18,6 @@ class VideoFormController implements Controller
             $video = $this->videoRepository->find($id);
         }
 
-        require_once __DIR__ . '../../../inicio-html.php'; ?>
-            <main class="container">
-                <form class="container__formulario"  
-                    method="post">
-                    <h2 class="formulario__titulo">Envie um vídeo!</h2>
-                        <div class="formulario__campo">
-                            <label class="campo__etiqueta" for="url">Link embed</label>
-                            <input name="url"
-                                value="<?= $video?->url; ?>" 
-                                class="campo__escrita" 
-                                required
-                                placeholder="Por exemplo: https://www.youtube.com/embed/FAY1K2aUg5g" 
-                                id='url' />
-                        </div>
-
-                        <div class="formulario__campo">
-                            <label class="campo__etiqueta" for="title">Titulo do vídeo</label>
-                            <input name="title" 
-                                value="<?= $video?->title; ?>"
-                                class="campo__escrita" 
-                                required 
-                                placeholder="Neste campo, dê o nome do vídeo"
-                                id='title' />
-                        </div>
-
-                        <input class="formulario__botao" type="submit" value="Enviar" />
-                </form>
-
-            </main>
-        <?php 
-        require_once __DIR__ . '../../../fim-html.php'; 
+        require_once __DIR__ . "../../../views/video-form.php";
     }
 }
