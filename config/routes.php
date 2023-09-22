026dc767b539f01e430189c6d43ee60bbcfdd2d6
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2023-09-22 12:12:07 -0300

    Criando novas rotas para a aplicação, com API e WebServices

--- config/routes.php
+++ config/routes.php
@@ -2,12 +2,14 @@
 
 use Alura\Mvc\Controller\DeleteVideoController;
 use Alura\Mvc\Controller\EditVideoController;
+use Alura\Mvc\Controller\JsonVideoListController;
 use Alura\Mvc\Controller\LoginFormController;
 use Alura\Mvc\Controller\NewVideoController;
 use Alura\Mvc\Controller\VideoFormController;
 use Alura\Mvc\Controller\VideoListController;
 use Alura\Mvc\Controller\LoginController;
 use Alura\Mvc\Controller\LogoutController;
+use Alura\Mvc\Controller\NewJsonVideoController;
 use Alura\Mvc\Controller\RemoveCapaController;
 
 return [
@@ -20,5 +22,7 @@ return [
     'GET|/remover-capa' => RemoveCapaController::class,
     'GET|/login' => LoginFormController::class,
     'POST|/login' => LoginController::class,
-    'GET|/logout' => LogoutController::class
+    'GET|/logout' => LogoutController::class,
+    'GET|/videos-json' => JsonVideoListController::class,
+    'POST|/videos' => NewJsonVideoController::class
 ];
