5f654fc7fd5ca0d2b97f672429a4090829ffc5d3
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2023-10-06 16:51:44 -0300

    Ajustes finais

--- cria-usuarios.php
+++ cria-usuarios.php
@@ -11,10 +11,3 @@ $statement = $pdo->prepare($sql);
 $statement->bindValue(1, $email);
 $statement->bindValue(2, $hash);
 $statement->execute();
-
-if (password_needs_rehash($userData['password'], PASSWORD_ARGON2ID)) {
-    $statement = $this->pdo->prepare('UPDATE users SET passwd = ? WHERE id = ?');
-    $statement->bindValue(1, password_hash($passwd, PASSWORD_ARGON2ID));
-    $statement->bindValue(2, $userData['id']);
-    $statement->execute();
-}
